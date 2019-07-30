40948fb69c53179fd24adeca8bc8e11fa8ecc807
liavaldetaro <liavaldetaro@gmail.com>
2019-07-30 21:23:05 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -90,9 +90,7 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'msmb_theme'
-import msmb_theme
-html_theme_path = [msmb_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
